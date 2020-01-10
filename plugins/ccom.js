25762e7e6fbaf28432612d8c34a8e938685c49af
Matt <matt@haxed.net>
2020-01-10 03:01:59 -0500

    ccoms.js updated
    ccoms.js updated

--- plugins/ccom.js
+++ plugins/ccom.js
@@ -6,6 +6,7 @@ let lastSend = 0;
 let coms = require("./data/ccom.json");
 let maps = require("./data/maps.json");
 let rated = false;
+let running = 0;
 
 const mod = {
 	hook_commands: [
@@ -13,14 +14,30 @@ const mod = {
 			if(e.args.length > 1){
 				if(e.args[1] == "add"){
 					if(e.args.length > 3){
+						if(e.args[2].substr(0,1) == ".") e.args[2] = e.args[2].substr(1);
+						
 						if(isBad(e.message)){
 							e.reply("Command rejected because it contains banned words");
-						}else if(e.args[2].match(/[^ -~]+/g) == null){
+						}else if(1 /*e.args[2].match(/[^ -~]+/g) == null*/){
 							const code = e.message.substr(11 + e.args[2].length);
+							let ccomCount = 0;
+							for(let i in coms){
+								if(coms[i].user.nick.toLowerCase() == e.from.nick.toLowerCase()){
+									ccomCount++;
+								}
+							}
+							if(ccomCount > 20){
+								e.reply("You've already added 20 ccoms. You're not allowed to add more.");
+								return;
+							}
 
 							//coms.push({command: args[3], code: code, user: e.from, date: Date.now()});
-							const uri = "http://a.haxed.net/test.php?from=" + base64(e.from.nick) + "&line=" + base64(e.message) + "&code=" + base64(code);
-							request.get(uri, function (error, response, body) {
+							const uri = "http://home.web/test.php?from=" + base64(e.from.nick) + "&line=" + base64(e.message) + "&code=" + base64(code);
+							
+							console.log(uri);
+							var to = 0;
+							var r = request.get(uri, function (error, response, body) {
+								clearTimeout(to);
 								if (error || response.statusCode != 200) {
 									console.log(error);
 									e.reply("Command failed compilation. for help see https://haxed.net/ccom.html");
@@ -45,6 +62,7 @@ const mod = {
 									}
 								}
 							});
+							to = setTimeout(function(){ r.abort(); return e.reply("Command failed compilation at TIME-LIMIT"); },6000);
 						}else{
 							e.reply("Error: you may not use non-printable characers in commands");
 						}
@@ -59,12 +77,24 @@ const mod = {
 							delete maps[e.args[2].toLowerCase()];
 							return e.reply("The map has been removed");
 						}
+
 						for(let i in coms){
 							if(coms[i].command.toLowerCase() == e.args[2].toLowerCase()){
+								let reply = "";
 								if(coms[i].user.nick == e.from.nick || e.admin){
+									for(let b in maps){
+										if(maps[b].toLowerCase() == e.args[2].toLowerCase()){
+											reply += "Map " + b + " has been removed. "
+											delete maps[b];
+										}
+									}
+									
 									coms.splice(i,1);
 									fs.writeFileSync('./plugins/data/ccom.json', JSON.stringify(coms), 'utf8');
-									return e.reply("Command has been removed");
+									
+									
+									
+									return e.reply(reply + " Command " + e.args[2] + " has been removed.");
 								}else{
 									return e.reply("You may not edit a command you didn't add");
 								}
@@ -73,6 +103,9 @@ const mod = {
 					}
 					e.reply("Command not found");
 				}else if(e.args[1] == "view"){
+					if(maps[e.args[2].toLowerCase()] != undefined){
+						return e.reply(e.args[2] + " is a mapped to " + maps[e.args[2].toLowerCase()]);
+					}
 					if(e.args.length == 3){
 						for(let i in coms){
 							if(coms[i].command.toLowerCase() == e.args[2].toLowerCase()){
@@ -113,8 +146,41 @@ const mod = {
 						}
 						e.reply("The ccom wasn't found");
 					}
+				}else if(e.args[1] == "list"){
+					if(e.args.length < 2){
+						e.reply("Invalid command usage. for help see https://haxed.net/ccom.html");
+					}else{
+						let ccomList = "";
+						if(e.args.length == 2 || e.args[2].length < 2) e.args[2] = e.from.nick; 
+						for(let i in coms){
+							if(coms[i].user.nick.toLowerCase() == e.args[2].toLowerCase()){
+								ccomList += e.prefix + coms[i].command + " ";
+							}
+						}
+						e.reply("ccoms added by " + e.args[2] + ": " + ccomList);
+					}
+				}else if(e.args[1] == "delete"){
+					return e.reply("you mean: remove");
+				}else if(e.args[1] == "del"){
+					return e.reply("you mean: remove");
+				}else if(e.args[1] == "show"){
+					return e.reply("you mean: view");
+				}else if(e.args[1] == "clear"){
+						let done = false;
+						while(done == false){
+							done = true;
+							for(let i in coms){
+								if(coms[i].user.nick.toLowerCase() == e.from.nick.toLowerCase()){
+									coms.splice(i,1);
+									done = false;
+									break;
+								}
+							}
+						}
+						e.reply("Your ccoms are cleared.");
 				}else{
-					e.reply("Invalid command usage. for help see https://haxed.net/ccom.html");
+					
+					e.reply("Invalid command usage. .ccom [add|remove|map|view|list]");
 				}
 			}
 		}}
@@ -124,7 +190,7 @@ const mod = {
 		if(e.hcmd) return;
 		if(rated) return;
 		//maps[e.args[2]]
-
+		
 		if(e.message.substr(0,1) == e.prefix){
 			if(maps[e.args[0].substr(1).toLowerCase()] != undefined){
 				e.args[0] = e.prefix + maps[e.args[0].substr(1).toLowerCase()];
@@ -134,9 +200,17 @@ const mod = {
 				console.log(coms[i].command.toLowerCase());
 				if(coms[i].command.toLowerCase() == e.args[0].substr(1).toLowerCase()){
 					console.log("ok2");
-					const uri = "http://a.haxed.net/test.php?from=" + base64(e.from.nick) + "&line=" + base64(e.message) + "&code=" + base64(coms[i].code);
+					if(running > 3){
+						rated = true;
+						setTimeout(function(){rated = false},5000);
+						return e.reply("too many tasks running at once.");
+					}
+					const uri = "http://home.web/test.php?from=" + base64(e.from.nick) + "&line=" + base64(e.message) + "&code=" + base64(coms[i].code);
 					console.log(uri);
-					request.get(uri, function (error, response, body) {
+					running += 1;
+
+					var r = request.get(uri, function (error, response, body) {
+						running -= 1;
 						if (error || response.statusCode != 200) {
 							e.reply("Command failed compilation");
 						}else{
@@ -158,6 +232,19 @@ const mod = {
 							}
 						}
 					});
+					 r.on('response',function(response){
+						 var a = 0;
+						 response.on('data', function(chunk) {
+							a += chunk.length;
+							if(a > 1024){
+								
+								r.abort();
+								return e.reply("The output is too large to send");
+							}
+							
+						 });
+					   });
+					
 				}
 			}
 		}
@@ -165,6 +252,7 @@ const mod = {
 }
 
 function base64(e){
+	//return Buffer.from(e).toString('base64');
 	return encodeURIComponent(e);
 }
 
